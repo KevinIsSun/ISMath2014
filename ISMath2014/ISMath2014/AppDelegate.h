eadb248f271f5a71a1be284459a7b6ac4c54c2cd
kevinissun <kevinissun@163.com>
2014-12-08 12:18:46 +0800

    程序初始化

---
+++ ISMath2014/ISMath2014/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  ISMath2014
+//
+//  Created by 孙振东 on 14/12/8.
+//  Copyright (c) 2014年 gbstudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ ISMath2014/ISMath2014/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ISMath2014
+//
+//  Created by 孙振东 on 14/12/8.
+//  Copyright (c) 2014年 gbstudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
